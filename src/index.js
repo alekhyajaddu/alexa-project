51fca6eff1ce6fcab891d9e62f006cf6e79110c0
alekhyajaddu <alekhyajaddu@gmail.com>
2020-04-18 14:53:53 -0500

    resolved errors

--- src/index.js
+++ src/index.js
@@ -10,13 +10,8 @@ let time = {
     "coffee&career":{
         "time_zone": "CST",
         "hour": "08:30"
-<<<<<<< HEAD
-    }
-    "NWOrchestra":{
-=======
     },
-    "NWOrchestre":{
->>>>>>> e42550ebd98d05c5f98c7ae03d1415a094391e05
+    "NWOrchestra":{
         "time_zone": "CST",
         "hour": "19:30"
     },
@@ -27,11 +22,7 @@ let time = {
     "Meditation":{
         "time_zone": "CST",
         "hour": "12:00"
-<<<<<<< HEAD
-    }
-=======
     },
->>>>>>> e42550ebd98d05c5f98c7ae03d1415a094391e05
     "International Coffee Hour":{
         "time_zone": "CST",
         "hour": "14:30"
@@ -47,13 +38,8 @@ let venue = {
     },
     "coffee&career":{
         "Location": "Career Services, Administration Building, NWMSU, MO."
-<<<<<<< HEAD
-    }
-    "NWOrchestra":{
-=======
     },
-    "NWOrchestre":{
->>>>>>> e42550ebd98d05c5f98c7ae03d1415a094391e05
+    "NWOrchestra":{
         "Location": "Ron Houston Center for Performing Arts, Maryville, MO."
     },
     "Dodgeball Tournament":{
@@ -72,43 +58,21 @@ let description = {
         "desc":"Individuals are invited to participate – anytime, anywhere – by clocking their times and then sending a selfie and photo of their results to helenk@nwmissouri.edu or by posting to the KXCV-KRNW Facebook page."
     },
     "stressbuster":{
-<<<<<<< HEAD
         "desc":"Learn to recognize and effectively manage stress in fun, healthy ways! All students, faculty and staff are welcome."
-    }
+    },
     "coffee&career":{
         "desc":"This is a come and go session focused on helping students find internships and full-time jobs. Students are welcome to attend at any stage in the search process. Career Services staff and student Career Ambassadors will be on hand to answer questions and help. Coffee and other drinks are available and free of charge for students who attend."
-    }
+    },
     "NWOrchestra":{
         "desc":"The Northwest Orchestra and the Symphonic Band perform at 7:30 p.m. in the Mary Linn Auditorium at the Ron Houston Center for the Performing Arts. The concert is free and open to the public."
-    }
+    },
     "Dodgeball Tournament":{
         "desc":"Campus Recreation is having a Dodgeball Tournament on April 21st in the SRC. Make sure and bring Bearcat cards to all games to check in. "
-    }
+    },
     "Meditation":{
         "desc":"These sessions introduce individuals to the ancient practices of mindfulness and meditation and allow participants to engage in the practice of meditation on a regular basis. Participants are encouraged to bring a pillow or thick towel to sit on."
-    }
+    },
     
-=======
-        "time_zone": "CST",
-        "hour": "16:00"
-    },
-    "coffee&career":{
-        "time_zone": "CST",
-        "hour": "08:30"
-    },
-    "NWOrchestre":{
-        "time_zone": "CST",
-        "hour": "19:30"
-    },
-    "Dodgeball Tournament":{
-        "time_zone": "CST",
-        "hour": "17:00"
-    },
-    "Meditation":{
-        "time_zone": "CST",
-        "hour": "12:00"
-    },
->>>>>>> e42550ebd98d05c5f98c7ae03d1415a094391e05
     "International Coffee Hour":{
         "desc":"The activity provides an opportunity for students, staff and faculty to connect. Make new friends from around the globe, connect with old friends and have fun."
     }
@@ -121,53 +85,30 @@ let date = {
         "date":"17"
     },
     "stressbuster":{
-<<<<<<< HEAD
         "year":"2020",
         "month":"april",
         "date":"20"
-    }
+    },
     "coffee&career":{
         "year":"2020",
         "month":"april",
         "date":"24"
-    }
+    },
     "NWOrchestra":{
         "year":"2020",
         "month":"april",
         "date":"23"
-    }
+    },
     "Dodgeball Tournament":{
         "year":"2020",
         "month":"april",
         "date":"21"
-    }
+    },
     "Meditation":{
         "year":"2020",
         "month":"april",
         "date":"22"
-    }
-    
-=======
-        "time_zone": "CST",
-        "hour": "16:00"
     },
-    "coffee&career":{
-        "time_zone": "CST",
-        "hour": "08:30"
-    },
-    "NWOrchestre":{
-        "time_zone": "CST",
-        "hour": "19:30"
-    },
-    "Dodgeball Tournament":{
-        "time_zone": "CST",
-        "hour": "17:00"
-    },
-    "Meditation":{
-        "time_zone": "CST",
-        "hour": "12:00"
-    },
->>>>>>> e42550ebd98d05c5f98c7ae03d1415a094391e05
     "International Coffee Hour":{
         "year":"2020",
         "month":"",
