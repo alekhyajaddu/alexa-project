ed8c5f6136a9123ef595b4dbcb1b05c801f771b5
annie0sc <annie0sc@gmail.com>
2020-04-19 10:55:52 -0500

    made some changes to the events invocation

--- src/index.js
+++ src/index.js
@@ -1,123 +1,3 @@
-/*
-let time = {
-    "KXCV-KRNW": {
-        "time_zone": "CST",
-        "hour": "09:30"
-    },
-    "stressbuster": {
-        "time_zone": "CST",
-        "hour": "16:00"
-    },
-    "coffee&career": {
-        "time_zone": "CST",
-        "hour": "08:30"
-    },
-    "NWOrchestra": {
-        "time_zone": "CST",
-        "hour": "19:30"
-    },
-    "Dodgeball Tournament": {
-        "time_zone": "CST",
-        "hour": "17:00"
-    },
-    "Meditation": {
-        "time_zone": "CST",
-        "hour": "12:00"
-    },
-    "International Coffee Hour": {
-        "time_zone": "CST",
-        "hour": "14:30"
-    }
-}
-
-let venue = {
-    "KXCV-KRNW": {
-        "location": "KXCV-KRNW Facebook Page."
-    },
-    "stressbuster": {
-        "location": "The Station, Maryville, MO."
-    },
-    "coffee&career": {
-        "location": "Career Services, Administration Building, NWMSU, MO."
-    },
-    "NWOrchestra": {
-        "location": "Ron Houston Center for Performing Arts, Maryville, MO."
-    },
-    "Dodgeball Tournament": {
-        "location": "Student Rec Center, NWMSU, MO."
-    },
-    "Meditation": {
-        "location": "JW Jones Student Union, NWMSU, MO."
-    },
-    "International Coffee Hour": {
-        "location": "BD Owens Library, NWMSU, MO."
-    }
-}
-
-let description = {
-    "KXCV-KRNW": {
-        "desc": "Individuals are invited to participate – anytime, anywhere – by clocking their times and then sending a selfie and photo of their results to helenk@nwmissouri.edu or by posting to the KXCV-KRNW Facebook page."
-    },
-    "stressbuster": {
-        "desc": "Learn to recognize and effectively manage stress in fun, healthy ways! All students, faculty and staff are welcome."
-    },
-    "coffee&career": {
-        "desc": "This is a come and go session focused on helping students find internships and full-time jobs. Students are welcome to attend at any stage in the search process. Career Services staff and student Career Ambassadors will be on hand to answer questions and help. Coffee and other drinks are available and free of charge for students who attend."
-    },
-    "NWOrchestra": {
-        "desc": "The Northwest Orchestra and the Symphonic Band perform at 7:30 p.m. in the Mary Linn Auditorium at the Ron Houston Center for the Performing Arts. The concert is free and open to the public."
-    },
-    "Dodgeball Tournament": {
-        "desc":"Campus Recreation is having a Dodgeball Tournament on April 21st in the SRC. Make sure and bring Bearcat cards to all games to check in. " 
-    },
-    "Meditation": {
-        "desc": "These sessions introduce individuals to the ancient practices of mindfulness and meditation and allow participants to engage in the practice of meditation on a regular basis. Participants are encouraged to bring a pillow or thick towel to sit on."
-    },
-
-    "International Coffee Hour": {
-        "desc": "The activity provides an opportunity for students, staff and faculty to connect. Make new friends from around the globe, connect with old friends and have fun."
-    }
-}
-
-let date = {
-    "KXCV-KRNW": {
-        "year": "2020",
-        "month": "april",
-        "day": "17"
-    },
-    "stressbuster": {
-        "year": "2020",
-        "month": "april",
-        "day": "20"
-    },
-    "coffee&career": {
-        "year": "2020",
-        "month": "april",
-        "day": "24"
-    },
-    "NWOrchestra": {
-        "year": "2020",
-        "month": "april",
-        "day": "23"
-    },
-    "Dodgeball Tournament": {
-        "year": "2020",
-        "month": "april",
-        "day": "21"
-    },
-    "Meditation": {
-        "year": "2020",
-        "month": "april",
-        "day": "22"
-    },
-    "International Coffee Hour": {
-        "year": "2020",
-        "month": "",
-        "day": "22"
-    }
-}
-*/
-
 const event = require('./events.json')
 
 // Route the incoming request based on type (LaunchRequest, IntentRequest,
@@ -185,13 +65,13 @@ function onIntent(IntentRequest, session, callback) {
     var intentName = IntentRequest.intent.name;
 
     // dispatch custom intents to handlers here
-    if (intentName == "time") {
+    if (intentName == EventSource.time) {
         handleTimeResponse(intent, session, callback)
-    } else if (intentName == "venue") {
+    } else if (intentName == EventSource.venue) {
         handleVenueResponse(intent, session, callback)
-    } else if (intentName == "description") {
+    } else if (intentName == EventSource.description) {
         handleDescriptionResponse(intent, session, callback)
-    } else if (intentName == "date") {
+    } else if (intentName == EventSource.date) {
         handleDateResponse(intent, session, callback)
     } else if (intentName == "AMAZON.FallbackIntent") {
         handleFallbackResponse(intent, session, callback)
